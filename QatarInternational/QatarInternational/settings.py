70be0dbece3e602617de2ba58cbd25b1a14b84cb
Md.Morsalin <57835317+morsalin101@users.noreply.github.com>
2025-04-06 19:55:40 +0600

    login

--- QatarInternational/QatarInternational/settings.py
+++ QatarInternational/QatarInternational/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'QatarInternational.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],    # Add this line to specify the templates directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- QatarInternational/QatarInternational/urls.py
+++ QatarInternational/QatarInternational/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import login_view
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_view, name='login'),
 ]
